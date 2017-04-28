de923a89d3fd06876b9ec59383e1735b77295e5a
samueljim <samueljim@msn.com>
2017-04-28 12:31:56 +1000

    better c

--- config/assets/production.js
+++ config/assets/production.js
@@ -8,7 +8,8 @@ module.exports = {
       css: [
         // bower:css
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap.min.css'
+        'public/lib/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
         // endbower
       ],
       js: [
