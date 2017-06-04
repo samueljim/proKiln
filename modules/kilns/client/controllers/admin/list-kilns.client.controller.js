e5205fe4e0a00f6d264f127ce34d7e275f570540
samueljim <samueljim@msn.com>
2017-06-04 22:30:33 +1000

    make it normal

---
+++ modules/kilns/client/controllers/admin/list-kilns.client.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('kilns.admin')
+    .controller('KilnsAdminListController', KilnsAdminListController);
+
+  KilnsAdminListController.$inject = ['KilnsService'];
+
+  function KilnsAdminListController(KilnsService) {
+    var vm = this;
+
+    vm.kilns = KilnsService.query();
+  }
+}());
