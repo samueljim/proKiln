526baebcdbb4623e1ae26c9f4367970bc8501d80
samueljim <samueljim@msn.com>
2017-06-16 08:05:01 +1000

    css 2.0

--- config/assets/production.js
+++ config/assets/production.js
@@ -9,7 +9,6 @@ module.exports = {
         // bower:css
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css',
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'modules/core/client/css/bootstrap.css'
         // endbower
       ],
       js: [
@@ -27,7 +26,8 @@ module.exports = {
         // endbower
       ]
     },
-    // css: 'public/dist/application*.min.css',
-    // js: 'public/dist/application*.min.js'
+    css: 'public/dist/application*.min.css',
+    js: 'public/dist/application*.min.js',
+    css: 'modules/core/client/css/bootstrap.css'
   }
 };
