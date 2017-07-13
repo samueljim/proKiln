8041c05550e04feacfb3177c8fb3d4744d1a4ad1
samueljim <samueljim@msn.com>
2017-07-13 14:58:59 +1000

    added controlpanel serach

--- modules/controlPanels/client/controllers/list-controlPanels.client.controller.js
+++ modules/controlPanels/client/controllers/list-controlPanels.client.controller.js
@@ -5,12 +5,43 @@
     .module('controlPanels')
     .controller('ControlPanelsListController', ControlPanelsListController);
 
-  ControlPanelsListController.$inject = ['ControlPanelsService'];
+  ControlPanelsListController.$inject = ['$scope', '$filter', 'ControlPanelsService'];
 
-  function ControlPanelsListController(ControlPanelsService) {
+  function ControlPanelsListController($scope, $filter, ControlPanelsService) {
     var vm = this;
 
-    vm.controlPanels = ControlPanelsService.query();
+    // vm.controlPanels = ControlPanelsService.query();
+
+    vm.buildPager = buildPager;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+    vm.pageChanged = pageChanged;
+
+    ControlPanelsService.query(function (data) {
+      vm.controlPanels = data;
+      vm.buildPager();
+    });
+
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 15;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.controlPanels, {
+        $: vm.search 
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }
+
     // function isOwnedByUser() {
     //   return vm.controlPanels.user === vm.authentication.user._id;
     // }
