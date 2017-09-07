01b04904f586114944b3c6872c59a54f632f66fa
samueljim <samueljim@msn.com>
2017-09-07 14:46:30 +1000

    it's a test

--- modules/controlPanels/client/controlPanels.client.module.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('controlPanels', ['core', 'chart.js']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('controlPanels.admin', ['core.admin']);
-  app.registerModule('controlPanels.admin.routes', ['core.admin.routes']);
-  app.registerModule('controlPanels.services');
-  app.registerModule('controlPanels.routes', ['ui.router', 'core.routes', 'controlPanels.services']);
-}(ApplicationConfiguration));
